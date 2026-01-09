8fd9a48edeeb9d5c22bcceb71d40425bb5ba9412
ravinderthakur9 <ravinderthakur9@gmail.com>
2026-01-10 00:52:31 +0530

    project1_09_01_2026

--- filehandling.py
+++ filehandling.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 def fileopen():
-    df = pd.read_excel("Test.xlsx")
+    df = pd.read_excel(r"C:\Users\Shiv_Ravinder_Singh\OneDrive - Dell Technologies\Documents\Python\Test.xlsx")
     return df
 
 fileopen()
